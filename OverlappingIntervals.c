59d905177df14c653a3b0e18fbf3758375a83b28
Vinit <gva401@gmail.com>
2024-12-08 17:15:06 +0530

    Update OverlappingIntervals.c

--- OverlappingIntervals.c
+++ OverlappingIntervals.c
@@ -1,1 +1,19 @@
 
+class Solution {
+  public:
+    vector<vector<int>> mergeOverlap(vector<vector<int>>& arr) {
+        // Code here
+        sort(arr.begin(), arr.end());
+        vector<vector<int>> ans = {};
+        for(int i=0; i<arr.size(); i++){
+            if(ans.empty() || arr[i][0] > ans.back()[1]){
+                ans.push_back(arr[i]);
+            }
+            else{
+                ans.back()[1] = max(ans.back()[1], arr[i][1]);
+            }
+        }
+        return ans;
+    }
+
+};
