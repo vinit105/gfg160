d3dd559d135bdeefe4c33be99bab931705cb45bb
vinit105 <161505072+vinit105@users.noreply.github.com>
2024-12-12 15:11:41 +0530

    added search target (problem)  file

---
+++ NumberOfOccurence.java
@@ -0,0 +1,64 @@
+public class NumberOfOccurence {
+    
+}
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+
+        while (tc-- > 0) {
+
+            String[] str = br.readLine().trim().split(" ");
+            int[] a = new int[str.length];
+            for (int i = 0; i < str.length; i++) {
+                a[i] = Integer.parseInt(str[i]);
+            }
+            String[] nk = br.readLine().trim().split(" ");
+            int k = Integer.parseInt(nk[0]);
+            Solution sln = new Solution();
+            int ans = sln.countFreq(a, k);
+
+            System.out.println(ans);
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+// Given a sorted array, arr[] and a number target, you need to find the number of occurrences of target in arr[]. 
+
+// Examples :
+
+// Input: arr[] = [1, 1, 2, 2, 2, 2, 3], target = 2
+// Output: 4
+// Explanation: target = 2 occurs 4 times in the given array so the output is 4.
+// Input: arr[] = [1, 1, 2, 2, 2, 2, 3], target = 4
+// Output: 0
+// Explanation: target = 4 is not present in the given array so the output is 0.
+// Input: arr[] = [8, 9, 10, 12, 12, 12], target = 12
+// Output: 3
+// Explanation: target = 12 occurs 3 times in the given array so the output is 3.
+
+class Solution {
+    int countFreq(int[] arr, int target) {
+        // code here
+        int count=0;
+        int n= arr.length;
+        if(target < arr[0] || target > arr[n-1]){ 
+            return count;
+            
+        }
+        for(int i=0; i<n; i++){
+            if(arr[i]==target){
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
