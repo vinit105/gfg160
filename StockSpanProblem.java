be825be2d85c3b976f81f32671dadb3ccd7ea876
vinit105 <161505072+vinit105@users.noreply.github.com>
2025-02-24 20:43:01 +0530

    added StockSpanProblem.java file

---
+++ StockSpanProblem.java
@@ -0,0 +1,59 @@
+public class StockSpanProblem {
+    
+}
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+
+        while (t-- > 0) {
+
+            ArrayList<Integer> array1 = new ArrayList<Integer>();
+            String line = read.readLine();
+            String[] tokens = line.split(" ");
+            for (String token : tokens) {
+                array1.add(Integer.parseInt(token));
+            }
+            ArrayList<Integer> v = new ArrayList<Integer>();
+            int[] arr = new int[array1.size()];
+            int idx = 0;
+            for (int i : array1) arr[idx++] = i;
+
+            v = new Solution().calculateSpan(arr);
+
+            for (int i = 0; i < v.size(); i++) System.out.print(v.get(i) + " ");
+
+            System.out.println();
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+
+class Solution {
+    public ArrayList<Integer> calculateSpan(int[] arr) {
+        // write code here
+        ArrayList<Integer> span = new ArrayList<>();
+        Stack<Integer> stack  = new Stack<>();
+        
+        for(int i = 0; i< arr.length; i++){
+            int days = 1;
+            while(!stack.isEmpty() && arr[stack.peek()] <= arr[i]){
+                days += span.get(stack.pop());
+            }
+            span.add(days);
+            stack.push(i);
+        }
+        return span;
+    }
+}
