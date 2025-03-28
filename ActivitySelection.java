f510a632623af1ba87b9d34af3551b67f4ae8cdb
vinit105 <161505072+vinit105@users.noreply.github.com>
2025-03-28 21:53:33 +0530

    added ActivitySelection.java file

---
+++ ActivitySelection.java
@@ -0,0 +1,57 @@
+public class ActivitySelection {
+    
+}
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(reader.readLine().trim());
+
+        while (t-- > 0) {
+            // Read the start times
+            String[] startInput = reader.readLine().trim().split("\\s+");
+            int[] start = new int[startInput.length];
+            for (int i = 0; i < startInput.length; i++) {
+                start[i] = Integer.parseInt(startInput[i]);
+            }
+
+            // Read the end times
+            String[] endInput = reader.readLine().trim().split("\\s+");
+            int[] finish = new int[endInput.length];
+            for (int i = 0; i < endInput.length; i++) {
+                finish[i] = Integer.parseInt(endInput[i]);
+            }
+
+            // Create solution object and call activitySelection
+            Solution obj = new Solution();
+            System.out.println(obj.activitySelection(start, finish));
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public int activitySelection(int[] start, int[] finish) {
+        // code here.
+        int n = start.length, ans = 0, fTime = -1;
+        Integer[] index = new Integer[n];
+        for(int i=0; i< n;i++){
+            index[i] = i;
+        }
+        Arrays.sort(index,Comparator.comparingInt(i -> finish[i]));
+        for(int i : index){
+            if(start[i] > fTime){
+                fTime = finish[i];
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
