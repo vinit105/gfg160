44d096878c2afca050e67b7260c60721fc2f4939
vinit105 <161505072+vinit105@users.noreply.github.com>
2025-04-02 22:03:48 +0530

    added BFS.java file

---
+++ BFS.java
@@ -0,0 +1,70 @@
+public class BFS {
+    
+}
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+
+// User function Template for Java
+class Solution {
+    // Function to return Breadth First Search Traversal of given graph.
+    public ArrayList<Integer> bfs(ArrayList<ArrayList<Integer>> adj) {
+        // code here
+        ArrayList<Integer> arr = new ArrayList<>();
+        boolean[] vertex = new boolean[adj.size()];
+        Queue<Integer> q = new LinkedList<>();
+        q.add(0);
+        vertex[0] = true;
+        while(!q.isEmpty()){
+            int i = q.poll();
+            arr.add(i);
+            for(int j : adj.get(i)){
+                if(!vertex[j]){
+                    vertex[j] = true;
+                    q.add(j);
+                }
+            }
+        }
+        return arr;
+    }
+}
+
+
+//{ Driver Code Starts.
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+
+        while (tc-- > 0) {
+            int V = Integer.parseInt(br.readLine().trim());
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+            for (int i = 0; i < V; i++) {
+                String[] input = br.readLine().trim().split(" ");
+                ArrayList<Integer> node = new ArrayList<>();
+                for (String s : input) {
+                    if (!s.isEmpty()) {
+                        node.add(Integer.parseInt(s));
+                    }
+                }
+                adj.add(node);
+            }
+
+            Solution obj = new Solution();
+            ArrayList<Integer> ans = obj.bfs(adj);
+            for (int num : ans) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
