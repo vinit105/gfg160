7a5a2b7d1a288c52b04b8e2d7b2567c8a3674f31
vinit105 <161505072+vinit105@users.noreply.github.com>
2025-04-20 20:57:33 +0530

    added ReapeatativeElement.java file

---
+++ RepeatativeElement.java
@@ -0,0 +1,50 @@
+public class RepeatativeElement {
+    
+}
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            // Read the array from input line
+            String inputLine = read.readLine().trim();
+            String[] inputArr = inputLine.split("\\s+");
+            int n = inputArr.length;
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputArr[i]);
+            }
+
+            Solution ob = new Solution();
+            System.out.println(ob.findDuplicate(arr));
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    public int findDuplicate(int[] arr) {
+        // code here
+        int s = arr[0] , f = arr[0];
+        do{
+            s = arr[s];
+            f =arr[arr[f]];
+        }while(s != f);
+        f = arr[0];
+        while(s != f){
+            s = arr[s];
+            f= arr[f];
+        }
+        return s ;
+    }
+}
