fcd60fa183cb510e8009efb7ee5ddee0e8ecae24
vinit105 <161505072+vinit105@users.noreply.github.com>
2025-05-02 21:07:24 +0530

    added ImplementThelowerbound.java  file

---
+++ ImplementLowBound.java
@@ -0,0 +1,50 @@
+public class ImplementLowBound {
+    
+}
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+
+        while (tc-- > 0) {
+
+            String[] str = br.readLine().trim().split(" ");
+            int[] a = new int[str.length];
+            for (int i = 0; i < str.length; i++) {
+                a[i] = Integer.parseInt(str[i]);
+            }
+            String[] nk = br.readLine().trim().split(" ");
+            int k = Integer.parseInt(nk[0]);
+            Solution sln = new Solution();
+            int ans = sln.lowerBound(a, k);
+
+            System.out.println(ans);
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    int lowerBound(int[] arr, int target) {
+        // code here
+        int low = 0, high = arr.length;
+        while(low < high ){
+            int mid = low + (high -  low)/2;
+            if(arr[mid] < target){
+                low = mid + 1;
+            }else{
+                high = mid;
+            }
+        }
+        return low;
+        
+    }
+}
